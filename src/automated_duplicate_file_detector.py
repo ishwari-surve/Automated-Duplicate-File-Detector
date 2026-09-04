b818b2d19fe4fd13286f8efa5cea6e3ff94d49ce
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-04 14:15:01 +0530

    Add automated_duplicate_file_detector.py with improved logic  - Add intelligent duplicate file detection using naming patterns - Delete only files with 'Copy' in filename - Better variable naming and error handling - Change default directory to 'Data' - Improved code structure and readability"

--- src/automated_duplicate_file_detector.py
+++ src/automated_duplicate_file_detector.py
@@ -1,94 +1,94 @@
 import hashlib
 import os
 
-def CalculateChecksum(FileName):          
-    fobj = open(FileName,"rb")
-
+def CalculateCheckSum(FileName):
+    fobj = open(FileName, "rb")
     hobj = hashlib.md5()
 
     Buffer = fobj.read(1000)
 
-    while(len(Buffer)> 0):
+    while(len(Buffer) > 0): 
         hobj.update(Buffer)
         Buffer = fobj.read(1000)
-    
+
     fobj.close()
 
     return hobj.hexdigest()
 
-def FindDuplicate(DirectoryName="target_directory"):
-    Ret = False
+def FindDuplicate(DirectoryName="Data"):
 
     Ret = os.path.exists(DirectoryName)
 
-    if(Ret == False):
+    if Ret == False:
         print("There is no such directory")
         return
-    
-    Ret = os.path.exists(DirectoryName)
 
+    Ret = os.path.isdir(DirectoryName)
 
-    if(Ret == False):
-        print("It is no such directory")
+    if Ret == False:
+        print("Provided path is not a directory")
         return
-    
-    Duplicate = {}
-    
-    for FolderName,SubFolderName,FileName in os.walk(DirectoryName):
-        for fname in FileName:
-            fname = os.path.join(FolderName,fname)
-            Checksum = CalculateChecksum(fname)
 
-            if Checksum in Duplicate:
-                Duplicate[Checksum].append(fname)
+    Duplicate = {}
+
+    for FolderName, SubFolderName, FileName in os.walk(DirectoryName):
+
+        for fname in FileName:
+
+            fname = os.path.join(FolderName, fname)
+
+            CheckSum = CalculateCheckSum(fname)
+
+            if CheckSum in Duplicate:
+                Duplicate[CheckSum].append(fname)
             else:
-                Duplicate[Checksum] = [fname]
-           
+                Duplicate[CheckSum] = [fname]
+
     return Duplicate
 
 def DisplayResult(MyDict):
 
-    Result = list(filter(lambda x : len(x) > 1,MyDict.values()))
+    Result = list(filter(lambda x: len(x) > 1, MyDict.values()))
 
     Count = 0
 
     for value in Result:
+
         for subvalue in value:
             Count = Count + 1
             print(subvalue)
 
-        print("Value of Count is:",Count)
+        print("Duplicate Files Count:", Count)
         Count = 0
 
-def DeleteDuplicate(Path = "target_directory"):
+def DeleteDuplicate(Path="Data"):
+
     MyDict = FindDuplicate(Path)
 
-    Result = list(filter(lambda x : len(x) > 1,MyDict.values()))
+    if MyDict is None:
+        return
 
-    Count = 0
-    Cnt = 0
+    Result = list(filter(lambda x: len(x) > 1, MyDict.values()))
+
+    DeletedCount = 0
 
     for value in Result:
+
         for subvalue in value:
-            Count = Count + 1
-            if(Count > 1):
-                print("Deleted file :",subvalue)
+
+            if " - Copy" in os.path.basename(subvalue):
+                print("Deleted File :", subvalue)
                 os.remove(subvalue)
-                Cnt = Cnt + 1
+                DeletedCount += 1
 
-        Count = 0
-
-    print("Total deleted files :",Cnt)
-            
-       
+    print("Total Deleted Files :", DeletedCount)
 def main():
-   DeleteDuplicate()
-   
-   Ret = FindDuplicate()
-   DisplayResult(Ret)
 
+    Ret = FindDuplicate()
 
+    if Ret is not None:
+        DisplayResult(Ret)
+        DeleteDuplicate()
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
